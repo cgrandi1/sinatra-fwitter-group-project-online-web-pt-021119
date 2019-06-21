3e94e2ca3131d713a5a5200f1aafe4bfd62a313d
Carlton Dwayne <carltondgrandison@gmail.com>
2019-06-21 02:32:35 +0000

    Automatically backed up by Learn

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -369,42 +369,43 @@ describe ApplicationController do
     end
   end
 
-  # describe 'delete action' do
-  #   context "logged in" do
-  #     it 'lets a user delete their own tweet if they are logged in' do
-  #       user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
-  #       tweet = Tweet.create(:content => "tweeting!", :user_id => 1)
-  #       visit '/login'
-  #
-  #       fill_in(:username, :with => "becky567")
-  #       fill_in(:password, :with => "kittens")
-  #       click_button 'submit'
-  #       visit 'tweets/1'
-  #       click_button "Delete Tweet"
-  #       expect(page.status_code).to eq(200)
-  #       expect(Tweet.find_by(:content => "tweeting!")).to eq(nil)
-  #     end
-  #
-  #
-  #     it 'does not let a user delete a tweet they did not create' do
-  #       user1 = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
-  #       tweet1 = Tweet.create(:content => "tweeting!", :user_id => user1.id)
-  #
-  #       user2 = User.create(:username => "silverstallion", :email => "silver@aol.com", :password => "horses")
-  #       tweet2 = Tweet.create(:content => "look at this tweet", :user_id => user2.id)
-  #
-  #       visit '/login'
-  #
-  #       fill_in(:username, :with => "becky567")
-  #       fill_in(:password, :with => "kittens")
-  #       click_button 'submit'
-  #       visit "tweets/#{tweet2.id}"
-  #       click_button "Delete Tweet"
-  #       expect(page.status_code).to eq(200)
-  #       expect(Tweet.find_by(:content => "look at this tweet")).to be_instance_of(Tweet)
-  #       expect(page.current_path).to include('/tweets')
-  #     end
-  #   end
+  describe 'delete action' do
+    context "logged in" do
+      it 'lets a user delete their own tweet if they are logged in' do
+        user = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+        tweet = Tweet.create(:content => "tweeting!", :user_id => 1)
+        visit '/login'
+
+        fill_in(:username, :with => "becky567")
+        fill_in(:password, :with => "kittens")
+        click_button 'submit'
+        visit 'tweets/1'
+        click_button "Delete Tweet"
+        expect(page.status_code).to eq(200)
+        expect(Tweet.find_by(:content => "tweeting!")).to eq(nil)
+      end
+    end
+
+
+    #   it 'does not let a user delete a tweet they did not create' do
+    #     user1 = User.create(:username => "becky567", :email => "starz@aol.com", :password => "kittens")
+    #     tweet1 = Tweet.create(:content => "tweeting!", :user_id => user1.id)
+    #
+    #     user2 = User.create(:username => "silverstallion", :email => "silver@aol.com", :password => "horses")
+    #     tweet2 = Tweet.create(:content => "look at this tweet", :user_id => user2.id)
+    #
+    #     visit '/login'
+    #
+    #     fill_in(:username, :with => "becky567")
+    #     fill_in(:password, :with => "kittens")
+    #     click_button 'submit'
+    #     visit "tweets/#{tweet2.id}"
+    #     click_button "Delete Tweet"
+    #     expect(page.status_code).to eq(200)
+    #     expect(Tweet.find_by(:content => "look at this tweet")).to be_instance_of(Tweet)
+    #     expect(page.current_path).to include('/tweets')
+    #   end
+    # end
 
     context "logged out" do
       it 'does not load let user delete a tweet if not logged in' do
